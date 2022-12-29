63640db0fe78c8d1e607511b6757517ef6b8e2ec
TemmieChan <temmiechanowo@gmail.com>
2022-12-29 20:59:03 +0100

    Beaucoup de mise a jour

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ let buttonClick = () => {
    };
    const link = `https://3hentai.net/d/${genNumber()}`;
    const text = document.getElementById("hentai");
-   text.innerHTML = `<a href=${link} style="color: black">Il a un petit secret</a>`;
+   text.innerHTML = `<a href=${link} style="color: white">Il a un petit secret</a>`;
+};
+
+let singe = () => {
+   const audio = new Audio("../sound/singe.mp3");
+   audio.play();
 };
