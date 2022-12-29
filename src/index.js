f93b92496cfcc2df218bed603f0316be62bf10f4
TemmieChan <temmiechanowo@gmail.com>
2022-12-29 20:25:56 +0100

    Mise en place d'un CSS et JS plus amélioration HTML

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let buttonClick = () => {
+   let genNumber = () => {
+      return Math.floor(1000 + Math.random() * 599500);
+   };
+   const link = `https://3hentai.net/d/${genNumber()}`;
+   const text = document.getElementById("hentai");
+   text.innerHTML = `<a href=${link} target="_blank">Hey</a>`;
+};
